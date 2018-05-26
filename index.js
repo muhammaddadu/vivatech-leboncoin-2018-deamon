6d358750f3acb1ad8c8873b7d1840861c41c1a32
Muhammad Dadu <muhammad@dadu.co>
2018-05-26 04:28:27 +0100

    make it work with linux

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ const CONTRACT_ADDRESS = '0xc789bc784db54ef91a0d3e051986f61e3367a57a';
 const NODE_PRIVATE_KEY = '588bab92cce6d95af7207b248f799907849e86dbe00f27a6870aa6a47430b5ed';
 const NODE_PUBLIC_KEY = '0xA7387feCcA51130A0F117C9cEec18287390E2bF2';
 
-const solc = require('solc');
+// const solc = require('solc');
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider(WEB3_PROVIDER));
 
 const BoxSource = fs.readFileSync(path.join(__dirname, 'smartcontracts/Box.sol'), 'utf8');
-const BoxCompiled = solc.compile({
-    sources: {
-        'BoxSource.sol': BoxSource
-    }
-}, 1).contracts['BoxSource.sol:Box'];
+// const BoxCompiled = solc.compile({
+//     sources: {
+//         'BoxSource.sol': BoxSource
+//     }
+// }, 1).contracts['BoxSource.sol:Box'];
+const BoxCompiled = require('./smartcontracts/Box.compiled.json');
 const Contract = web3.eth.contract(JSON.parse(BoxCompiled.interface));
 
 fs.writeFileSync(path.join(__dirname, 'smartcontracts/Box.compiled.json'), JSON.stringify(BoxCompiled));
@@ -25,7 +26,7 @@ function watchContractAtAddress(contractAddress) {
     let contractFields = ['owner', 'pricePerDay', 'lat', 'long', 'productContract', 'getStatus'];
     let responsesWanted = contractFields.length;
 
-    let data = {};
+    let _data = {};
 
     async.forEach(contractFields, (key, done) => {
         myContract[key]((err, value) => {
@@ -33,13 +34,16 @@ function watchContractAtAddress(contractAddress) {
                 console.error(err);
             }
 
-            data[key] = value;
+            _data[key] = value;
             done();
         });
     }, function () {
         setTimeout(() => {
-            watchContractAtAddress();
+            watchContractAtAddress(CONTRACT_ADDRESS);
         }, 1000);
+
+        let data = JSON.parse(JSON.stringify(_data));
+        console.log(data);
         
         switch (data.getStatus) {
             case '0': // AVAILABLE
@@ -47,7 +51,7 @@ function watchContractAtAddress(contractAddress) {
 
                 break;
             case '1': // Locked
-                
+
                 break;
         }
     });
