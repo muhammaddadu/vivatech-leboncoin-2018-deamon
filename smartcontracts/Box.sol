86a5495c9c86cc5dff3c010c570054e3e8d658b3
a <adaurkin@rambler.ru>
2018-05-26 04:45:01 +0200

    Update smart contract

--- smartcontracts/Box.sol
+++ smartcontracts/Box.sol
@@ -3,22 +3,35 @@ pragma solidity ^0.4.23;
 contract Box {
   address public owner;
   uint public pricePerDay; // in wei?
-  int public lat; // lattitude multiplied by 1000?
-  int public long; // longitude multiplied by 1000?
+  int public lat; // lattitude multiplied by 10 000?
+  int public long; // longitude multiplied by 10 000?
+  string dimentions; // L*W*H in cm
   address public productContract;
   
-  enum state {AVAILABLE, LOCKED} // , RESERVED, ...?
+  enum state {AVAILABLE, LOCKED, UNLOCKED} // , RESERVED, ...?
   state status;
 
-  constructor() public {
+  constructor(
+    uint _pricePerDay,
+    int _lat,
+    int _long,
+    string _dimentions
+  ) public {
     owner = msg.sender;
     status = state.AVAILABLE;
-    // to do: add parameters and assign them to members
+    pricePerDay = _pricePerDay;
+    lat = _lat;
+    long = _long;
+    dimentions = _dimentions;
   }
 
-  // will we us it?
   modifier restricted() {
     if (msg.sender == owner) _;
+  }
+
+  function update() public restricted returns (bool) {
+    // copy from constructor
+    return true;
   }
 
   function getStatus() public view returns (state) {
@@ -37,5 +50,16 @@ contract Box {
     require(status == state.AVAILABLE);
     productContract = _productContract;
     status = state.LOCKED;
+  }
+
+  // unlock the box for your deal
+  function release() public {
+    // check status
+    require(status == state.LOCKED);
+    // check if the calling user is authirised or the status of product contract?
+    // require(productContract == msg.sender);
+    productContract = 0;
+    // status = state.UNLOCKED; // ???
+    status = state.AVAILABLE;
   }
 }
