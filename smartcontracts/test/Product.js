ba4ab9890298aa426cb91855fd7a5ca32d4df441
a <adaurkin@rambler.ru>
2018-05-26 10:53:49 +0200

    Add tests for smart contracts

---
+++ smartcontracts/test/Product.js
@@ -0,0 +1,26 @@
+const Product = artifacts.require("Product.sol");
+
+contract('Product', async (accounts) => {
+
+  var state = {
+    NOTINBOX: 0,
+    INBOX: 1,
+    SOLD: 2,
+    TAKEN: 3
+  };
+
+  it("simple test", async () => {
+    let instance = await Product.new("Product 1", 1000000, "https://leboncoin.fr/", 0);
+
+    assert.equal(state.NOTINBOX, (await instance.getStatus()).valueOf());
+
+    await instance.setStatus(state.INBOX);
+    assert.equal(state.INBOX, (await instance.getStatus()).valueOf());
+
+    await instance.setStatus(state.SOLD);
+    assert.equal(state.SOLD, (await instance.getStatus()).valueOf());
+
+    await instance.setStatus(state.TAKEN);
+    assert.equal(state.TAKEN, (await instance.getStatus()).valueOf());
+  })
+});
